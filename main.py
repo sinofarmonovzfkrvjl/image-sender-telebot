ad00c11c0da34eccf8f7da323ecb5452696c23ce
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2025-02-20 18:43:30 +0500

    updated

--- main.py
+++ main.py
@@ -63,29 +63,29 @@ def check_status(message: types.Message):
         markup.add(InlineKeyboardButton("📸 qolgan rasmlarni ko'rish", callback_data="photo_count"),
                InlineKeyboardButton("📤 Rasmlar qancha postga yetishini ko'rish", callback_data="post_count"))
 
-        bot.send_message(message.chat.id, f"Salom {message.from_user.full_name}\n📊 Bot Status:", reply_markup=markup)
+        bot.send_message(message.chat.id, f"Salom {message.from_user.full_name}\n\n📊 Bot Status:", reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: call.data in ["photo_count", "post_count"])
 def see_bot_status(call: types.CallbackQuery):
-    logging.info("handled")
+    print("handled")
     os.path.join(IMAGE_FOLDER)
-    logging.info("photos folder is joined")
+    print("photos folder is joined")
     photo_count = len([img for img in os.listdir(IMAGE_FOLDER) if img.lower().endswith(("jpg", "jpeg", "png", "webp"))])
-    logging.info(f"length of photos: {photo_count}")
+    print(f"length of photos: {photo_count}")
     if photo_count == 0 and photo_count % 9 == 0:
-        logging.info("photo count is 0")
+        print("photo count is 0")
         post_count = 0
     else:
-        logging.info("photo count is not 0")
+        print("photo count is not 0")
         post_count = photo_count // 9
-    logging.info("post count is calculated")
+    print("post count is calculated")
     if call.data == "photo_count":
-        logging.info("photo count")
+        print("photo count")
         bot.answer_callback_query(call.id, f"📸 Qolgan rasmlar: {photo_count}", show_alert=True)
     elif call.data == "post_count":
-        logging.info("post count")
+        print("post count")
         bot.answer_callback_query(call.id, f"📤 Post qilishga yetadi: {post_count} marta", show_alert=True)
-    logging.info("answer sent")
+    print("answer sent")
     bot.answer_callback_query(call.id)
 
 @bot.message_handler(commands=['delete'])
